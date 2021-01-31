e00daadaba469cf6cf3df94aa6f22d6d38da2822
askarkaka@icloud.com <9GX(y?C4Ajxua>
2021-01-31 18:27:22 +0300

    initial win commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import javafx.stage.Stage;
+
+public class Main extends javafx.application.Application{
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+        primaryStage.setTitle("Dangerous Weapon");
+//        InputStream iconStream = getClass().getResourceAsStream("icon.png");
+//        Image image = new Image(iconStream);
+//        primaryStage.getIcons().add(image);
+    }
+}
